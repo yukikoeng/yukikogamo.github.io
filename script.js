45e93897a1ebaef8a136887770eeff5d6882a790
yukikogamo <yukiko@gmail.com>
2024-12-28 21:19:52 +0900

    サイト修正

--- script.js
+++ script.js
@@ -1,8 +1,3 @@
-/**
- * Basic JavaScript example.
- * Feel free to add menu toggles, animations, or other features here.
- */
-
 document.addEventListener("DOMContentLoaded", () => {
     console.log("Welcome to my portfolio website!");
   });
